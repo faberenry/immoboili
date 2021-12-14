f11bd3a0c0d9396b04eb6915258cb7f27c88198e
faberenry <enrico.favretto-1@studenti.unitn.it>
2021-12-14 22:36:42 +0100

    Modifica cosette
    Eliminati file inutili, cambiato il path di alcune risorse, modificato il main e aggiunto il css

--- routes/annunci.js
+++ routes/annunci.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   //visualizzazione di tutti gli annunci
+  //bisogna prendere la collectin:Annunci e visualizzarli tutti
   res.render('annunci/index');
 });
 
